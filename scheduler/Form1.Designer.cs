3fec1f4dffe60d265be24459c431ef840ed5090d
Spud <Spud@DESKTOP-DISQ3JF>
2022-08-02 16:40:58 +0100

    Add project files.

---
+++ scheduler/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace scheduler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.selectAllButton = new System.Windows.Forms.Button();
+            this.deselectAllButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.label1.Location = new System.Drawing.Point(25, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(537, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Code Ninjas Scheduler";
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(144)))), ((int)(((byte)(191)))));
+            this.checkedListBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox.CheckOnClick = true;
+            this.checkedListBox.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(23, 221);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(585, 378);
+            this.checkedListBox.TabIndex = 1;
+            // 
+            // selectAllButton
+            // 
+            this.selectAllButton.AutoSize = true;
+            this.selectAllButton.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectAllButton.Location = new System.Drawing.Point(23, 182);
+            this.selectAllButton.Name = "selectAllButton";
+            this.selectAllButton.Size = new System.Drawing.Size(89, 33);
+            this.selectAllButton.TabIndex = 2;
+            this.selectAllButton.Text = "Select All";
+            this.selectAllButton.UseVisualStyleBackColor = true;
+            this.selectAllButton.Click += new System.EventHandler(this.selectAllButton_Click);
+            // 
+            // deselectAllButton
+            // 
+            this.deselectAllButton.AutoSize = true;
+            this.deselectAllButton.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deselectAllButton.Location = new System.Drawing.Point(118, 182);
+            this.deselectAllButton.Name = "deselectAllButton";
+            this.deselectAllButton.Size = new System.Drawing.Size(89, 33);
+            this.deselectAllButton.TabIndex = 3;
+            this.deselectAllButton.Text = "Deselect All";
+            this.deselectAllButton.UseVisualStyleBackColor = true;
+            this.deselectAllButton.Click += new System.EventHandler(this.deselectAllButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.AutoSize = true;
+            this.submitButton.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(289, 182);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(123, 38);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(144)))), ((int)(((byte)(191)))));
+            this.BackgroundImage = global::scheduler.Properties.Resources.looohoo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(608, 609);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.deselectAllButton);
+            this.Controls.Add(this.selectAllButton);
+            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "b";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.Button selectAllButton;
+        private System.Windows.Forms.Button deselectAllButton;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
